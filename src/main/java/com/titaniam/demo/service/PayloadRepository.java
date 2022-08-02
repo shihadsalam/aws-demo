03ce10298a682c6d639b18adc53089bf93613ec1
Shihad <34054298+shihadsalam@users.noreply.github.com>
2022-08-02 14:25:25 +0530

    Delete src/main directory

--- src/main/java/com/titaniam/demo/service/PayloadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.titaniam.demo.service;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.titaniam.demo.model.Payload;
-
-public interface PayloadRepository extends MongoRepository<Payload, String> {
-
-}
